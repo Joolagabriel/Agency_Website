c0fb567dbbe5e89590a80835dd739726525f4678
Joolagabriel <joshua.olagabriel@gmail.com>
2025-02-01 20:24:49 -0700

    remove logo.png file and change import syntax

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Phone, Mail, MapPin } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpg';
 
 export default function Footer() {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, ChevronDown } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpg';
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
