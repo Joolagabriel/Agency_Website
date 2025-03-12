6e33e2f9ee1b81b2cea142cd56614ecf639fdaa4
Joshua <joshua.olagabriel@gmail.com>
2025-03-11 23:06:51 -0600

    change to PNG file extension in footer page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Phone, Mail, MapPin } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 
 export default function Footer() {
   const scrollToTop = () => {
